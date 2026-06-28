b86372dffc65ac08e0c8e04dee230edd28e9692a
dilshan_k <dilshan_k@epiclanka.net>
2026-06-29 02:53:59 +0530

    activeity log repor add

---
+++ src/main/java/lk/di47/ticket/repository/ActivityLogRepository.java
@@ -0,0 +1,12 @@
+package lk.di47.ticket.repository;
+
+import lk.di47.ticket.entity.ActivityLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
+    List<ActivityLog> findTop100ByOrderByCreatedAtDesc();
+
+    List<ActivityLog> findTop10ByOrderByCreatedAtDesc();
+}
