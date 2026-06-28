84b0f4c532f270a9631e4c6374541ee56337599c
dilshan_k <dilshan_k@epiclanka.net>
2026-06-29 01:20:36 +0530

    init

---
+++ src/main/java/lk/di47/ticket/SupportTicketBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.di47.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTicketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportTicketBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/di47/ticket/SupportTicketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.di47.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTicketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
