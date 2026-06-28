5507f04de661412f67aa83560ca3d8a44acdda09
dilshan_k <dilshan_k@epiclanka.net>
2026-06-29 02:24:36 +0530

    add crypto constant

---
+++ src/main/java/lk/di47/ticket/constant/CryptoConstant.java
@@ -0,0 +1,9 @@
+package lk.di47.ticket.constant;
+
+public final class CryptoConstant {
+    private CryptoConstant() {}
+
+    public static final String DECRYPTED_REQUEST_BODY = "DECRYPTED_REQUEST_BODY";
+    public static final String PLAIN_RESPONSE_BODY = "PLAIN_RESPONSE_BODY";
+    public static final String TRACE_ID = "TRACE_ID";
+}
