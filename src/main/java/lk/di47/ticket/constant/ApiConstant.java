02cf7d667210567a3efb22d4487864409c519d1a
dilshan_k <dilshan_k@epiclanka.net>
2026-06-29 02:23:22 +0530

    add constant

---
+++ src/main/java/lk/di47/ticket/constant/ApiConstant.java
@@ -0,0 +1,9 @@
+package lk.di47.ticket.constant;
+
+public final class ApiConstant {
+    private ApiConstant() {}
+
+    public static final String API = "/api";
+    public static final String VERSION_1 = "/v1";
+    public static final String API_V1 = API + VERSION_1;
+}
---
+++ src/main/java/lk/di47/ticket/response/ApiResponse.java
@@ -0,0 +1,18 @@
+package lk.di47.ticket.response;
+
+import java.time.LocalDateTime;
+
+public record ApiResponse<T>(
+        boolean success,
+        String message,
+        T data,
+        LocalDateTime timestamp
+) {
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data, LocalDateTime.now());
+    }
+
+    public static <T> ApiResponse<T> failed(String message, T data) {
+        return new ApiResponse<>(false, message, data, LocalDateTime.now());
+    }
+}
