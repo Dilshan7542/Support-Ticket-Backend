ac632d1fdfbda8de53c7ca79974f7027b8fe182b
dilshan_k <dilshan_k@epiclanka.net>
2026-06-29 03:43:48 +0530

    deparment list add

---
+++ src/main/java/lk/di47/ticket/feature/department/dto/ListDepartmentRequest.java
@@ -0,0 +1,9 @@
+package lk.di47.ticket.feature.department.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public record ListDepartmentRequest(
+        @NotNull(message = "userId is required")
+        Long userId
+) {
+}
